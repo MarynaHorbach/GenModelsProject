df3ce22847c2907e9eda4be4171850fec75072f8
Marina Romanova <marinar14112003@gmail.com>
2024-04-12 14:58:02 +0200

    loss classes mistake corrections 8

--- losses/GeneratorLoss.py
+++ losses/GeneratorLoss.py
@@ -29,6 +29,7 @@ class AdversarialLoss(nn.Module):
 
 class IdentityPreservationLoss(nn.Module):
 	def __init__(self, ArcFace):
+		super.__init__()
 		self.model = ArcFace
 		self.loss = nn.CosineEmbeddingLoss()
 
